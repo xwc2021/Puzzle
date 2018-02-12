6c5af1898076cb6613b16a25bc5e51a1dc912581
DESKTOP-Q76JGGA\xiang <xiangwei71@gmail.com>
2018-02-12 21:51:13 +0800

    更正前一版本的錯誤

--- Assets/Scripts/ScreenAdapter.cs
+++ Assets/Scripts/ScreenAdapter.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class ScreenAdapter : MonoBehaviour {
 
     [SerializeField]
-    float percentage=0.6f;//佔銀幕的百分比
+    float scale=0.6f;//放進銀幕了，可以再縮放一次
 
     public float GetScaleToFitScreen(ImgLoader imgLoader)
     {
+
         var imgRatio =imgLoader.GetRatio();
         var screenRatio = GetScreenRatio();
 
         //比銀幕大，就縮小到可以放進銀幕
-        var adapterScale = (imgRatio > screenRatio)?imgRatio / screenRatio:1.0f;
+        var adapterScale = (imgRatio > screenRatio)? screenRatio/ imgRatio : 1.0f;
 
-        return adapterScale*percentage;
+        return adapterScale* scale;
     }
 
     float GetScreenRatio()
